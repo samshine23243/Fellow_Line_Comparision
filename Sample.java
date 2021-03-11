8f62c64311c5b30b676db14b70c860231479ec1c
samshine <sameershinde23243@gmail.com>
2021-03-11 12:58:56 +0530

    [sameer] UC1 code added

--- Sample.java
+++ Sample.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
 
 public class Sample {
+	int x1,x2,y1,y2;
+	Scanner sc=new Scanner(System.in);
+	void input()
+	{
+		System.out.println("Enter x co-ordinate for first point:");
+		x1=sc.nextInt();
+		System.out.println("Enter y co-ordinate for first point:");
+		y1=sc.nextInt();
+		System.out.println("Enter x co-ordinate for second point:");
+		x2=sc.nextInt();
+		System.out.println("Enter y co-ordinate for second point:");
+		y2=sc.nextInt();
+	}
+	double calculate_length()
+	{
+		input();
+		int temp1=x2-x1;
+		int temp2=y2-y1;
+		double length=Math.sqrt((Math.pow(temp1, 2)+ Math.pow(temp2, 2)));
+		return length;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("welcome to line comparision computation program!!!");
-
+		Sample obj=new Sample();
+		//System.out.println(obj.calculate_length());
 	}
 
 }
